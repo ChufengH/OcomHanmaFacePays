65ef2f8b539d188f9f0318afd547e1a7b22ea671
flykule <Casltliu40@gmail.com>
2019-12-19 13:20:51 +0800

    增加日志上报功能

--- app/src/main/java/com/ocom/hanmafacepay/ui/act/RefundOrderListActivity.kt
+++ app/src/main/java/com/ocom/hanmafacepay/ui/act/RefundOrderListActivity.kt
@@ -1,7 +1,11 @@
 package com.ocom.hanmafacepay.ui.act
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,6 +13,7 @@ import com.example.android.observability.Injection
 import com.google.gson.Gson
 import com.ocom.faceidentification.base.BaseKeybroadActivity
 import com.ocom.hanmafacepay.R
+import com.ocom.hanmafacepay.const.CommonProcess
 import com.ocom.hanmafacepay.const.DEVICE_NUMBER
 import com.ocom.hanmafacepay.const.SIGN
 import com.ocom.hanmafacepay.const.TIME_STAMP
@@ -18,6 +23,7 @@ import com.ocom.hanmafacepay.network.entity.CancelOrderRequest
 import com.ocom.hanmafacepay.network.entity.Order
 import com.ocom.hanmafacepay.ui.adapter.OrderListAdapter
 import com.ocom.hanmafacepay.ui.widget.LoadingDialog
+import com.ocom.hanmafacepay.util.ToastUtil
 import com.ocom.hanmafacepay.util.extension.log
 import com.ocom.hanmafacepay.util.keyboard.Keyboard3
 import com.ocom.hanmafacepay.viewmodel.UserViewModel
@@ -32,7 +38,8 @@ import kotlin.math.min
 /**
  * 退款订单页面
  */
-class RefundOrderListActivity : BaseKeybroadActivity(), IHomeView, OrderListAdapter.OrderListAdapterView {
+class RefundOrderListActivity : BaseKeybroadActivity(), IHomeView,
+    OrderListAdapter.OrderListAdapterView {
 
     private lateinit var viewModelFactory: ViewModelFactory
     private lateinit var viewModel: UserViewModel
@@ -68,7 +75,8 @@ class RefundOrderListActivity : BaseKeybroadActivity(), IHomeView, OrderListAdap
                         val currentPos = manager.getPosition(focusedChild)
                         val pos = max(0, currentPos - 1)
                         recycler_view.scrollToPosition(pos)
-                        recycler_view.findViewHolderForAdapterPosition(pos)?.itemView?.requestFocus()
+                        recycler_view.findViewHolderForAdapterPosition(pos)
+                            ?.itemView?.requestFocus()
                     }
                 }
             }
@@ -79,7 +87,8 @@ class RefundOrderListActivity : BaseKeybroadActivity(), IHomeView, OrderListAdap
                         focusedChild ?: return@runOnUiThread
                         val manager = recycler_view.layoutManager as LinearLayoutManager
                         val currentPos = manager.getPosition(focusedChild)
-                        recycler_view.findViewHolderForAdapterPosition(currentPos)?.itemView?.performClick()
+                        recycler_view.findViewHolderForAdapterPosition(currentPos)
+                            ?.itemView?.performClick()
                     }
                 }
             }
@@ -152,7 +161,8 @@ class RefundOrderListActivity : BaseKeybroadActivity(), IHomeView, OrderListAdap
                         recycler_view.visibility = View.VISIBLE
                         mAdapter = OrderListAdapter(it, this)
                         recycler_view.post {
-                            recycler_view.findViewHolderForAdapterPosition(0)?.itemView?.requestFocus()
+                            recycler_view.findViewHolderForAdapterPosition(0)
+                                ?.itemView?.requestFocus()
                         }
                     }
                 })
@@ -160,11 +170,24 @@ class RefundOrderListActivity : BaseKeybroadActivity(), IHomeView, OrderListAdap
 
     override fun setAttachLayoutRes() = R.layout.activity_order_list
 
-    override fun onRefund(order: Order) {
+    fun refundOrder(order: Order) {
         mLoadingDialog.show()
-        val request = CancelOrderRequest(DEVICE_NUMBER, order.orderNo, order.user_id, TIME_STAMP, SIGN)
+        val request =
+            CancelOrderRequest(DEVICE_NUMBER, order.orderNo, order.user_id, TIME_STAMP, SIGN)
         log("开始退款:${Gson().toJson(request)}")
         mDataSource.cancelOrder(request)
+        mRefundOrder = null
+    }
+
+    private var mRefundOrder: Order? = null
+
+    override fun onRefund(order: Order) {
+        if (mIsAdmin) {
+            refundOrder(order)
+        } else {
+            mRefundOrder = order
+            mPwdInputDialog.show()
+        }
     }
 
     override fun onCancelOrderSuccess(orderNo: String) {
@@ -186,7 +209,7 @@ class RefundOrderListActivity : BaseKeybroadActivity(), IHomeView, OrderListAdap
         readTTs("退款失败$erroMsg")
     }
 
-    private fun commonNetError(){
+    private fun commonNetError() {
         readTTs("退款失败,请稍后重试")
         mLoadingDialog.dismiss()
     }
@@ -204,5 +227,86 @@ class RefundOrderListActivity : BaseKeybroadActivity(), IHomeView, OrderListAdap
     override fun onUnknownError(message: String) {
         super.onUnknownError(message)
         commonNetError()
+    }
+
+    private val mPwdInputDialog: AlertDialog by lazy { createPasswordInputDialog() }
+    private val mSettingDialog: AlertDialog by lazy { createPasswordSettingDialog() }
+    private var mInputPassword: String? = null//输入的密码
+    private var mPwdInputTv: EditText? = null //输入密码的editText
+    private var mIsAdmin = false
+
+    private fun createPasswordInputDialog(): AlertDialog {
+        val builder = AlertDialog.Builder(this@RefundOrderListActivity, R.style.lightDialog)
+        builder.setTitle("验证管理权限")
+        val viewInflated =
+            LayoutInflater.from(this@RefundOrderListActivity)
+                .inflate(R.layout.dialog_input_admin, null)
+        mPwdInputTv = viewInflated.findViewById(R.id.tv_input_password) as EditText
+        mPwdInputTv!!.setSelection(mPwdInputTv!!.text.length)
+        builder.setView(viewInflated)
+        builder.setPositiveButton(android.R.string.ok) { _, _ ->
+            mPwdInputDialog.dismiss()
+            mInputPassword = mPwdInputTv?.text.toString()
+            mPwdInputTv?.setText("")
+            mIsAdmin = TextUtils.equals(mInputPassword, CommonProcess.settingPassword)
+            if (mIsAdmin) {
+                mRefundOrder?.run {
+                    refundOrder(this)
+                }
+//                start(TencentSettingActivity::class.java)
+                mIsAdmin = false
+            } else {
+                ToastUtil.showLongToastCenter("密码错误!")
+            }
+        }
+
+        builder.setNeutralButton("修改密码") { _, _ ->
+            mPwdInputDialog.dismiss()
+            mSettingDialog.show()
+        }
+        builder.setNegativeButton(
+            android.R.string.cancel
+        ) { dialog, which -> dialog.cancel() }
+        return builder.create()
+    }
+
+    private fun createPasswordSettingDialog(): AlertDialog {
+        val builder = AlertDialog.Builder(this@RefundOrderListActivity, R.style.lightDialog)
+        builder.setTitle("修改密码")
+        val viewInflated =
+            LayoutInflater.from(this@RefundOrderListActivity)
+                .inflate(R.layout.dialog_input_set_pwd, null)
+        val oldTv = viewInflated.findViewById(R.id.tv_old_password) as EditText
+        val newTv = viewInflated.findViewById(R.id.tv_new_password) as EditText
+        oldTv.hint = "请输入旧密码"
+        builder.setView(viewInflated)
+        builder.setPositiveButton(android.R.string.ok) { _, _ ->
+            checkPassword(oldTv, newTv)
+        }
+        builder.setNeutralButton("默认密码") { _, _ ->
+            CommonProcess.settingPassword = "123321"
+            ToastUtil.showLongToastCenter("密码已恢复!")
+        }
+        builder.setNegativeButton(
+            android.R.string.cancel
+        ) { dialog, which -> dialog.cancel() }
+        return builder.create()
+    }
+
+    private fun checkPassword(oldTv: EditText, newTv: EditText) {
+        if (TextUtils.isEmpty(oldTv.text.toString())) {
+            ToastUtil.showShortToast("请输入正确的旧密码!")
+        } else {
+            if (TextUtils.isEmpty(newTv.text.toString()) || newTv.text.length < 6) {
+                ToastUtil.showShortToast("请输入新密码,最短为6位!")
+            } else {
+                if (!TextUtils.equals(oldTv.text.toString(), CommonProcess.settingPassword)) {
+                    ToastUtil.showShortToast("旧密码不正确!")
+                } else {
+                    ToastUtil.showLongToastCenter("密码设置成功!")
+                    CommonProcess.settingPassword = newTv.text.toString()
+                }
+            }
+        }
     }
 }
